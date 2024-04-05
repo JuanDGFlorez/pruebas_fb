68cb77a5780b11307a41743af6a6e6c9e95648a9
JuanDGFlorez <36492239+JuanDGFlorez@users.noreply.github.com>
2024-04-05 15:45:06 -0500

    Update stremlitt.py

--- stremlitt.py
+++ stremlitt.py
@@ -3,3 +3,14 @@ import pandas as pd
 import numpy as np
 
 
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM actualizacion_cityland.floridablanca.lc_terreno lt ', ttl="10m")
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
